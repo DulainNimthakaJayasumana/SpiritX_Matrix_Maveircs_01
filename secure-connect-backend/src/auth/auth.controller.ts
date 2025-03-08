78380772b09da473c14d6147c31f821c9e73e6ca
Akash Welgama <139337308+Akashwelgama@users.noreply.github.com>
2025-03-08 14:45:25 +0530

    secure-connect-backend-version-04
    Basic authentication methods set
    (Not tested)

--- secure-connect-backend/src/auth/auth.controller.ts
+++ secure-connect-backend/src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthDto} from "./AuthDTO";
+import {AuthService} from "./auth.service";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(
+        private authService: AuthService
+    ){}
+
+
+    @Post('signUp')
+    signUp(@Body() dto: AuthDto){
+
+
+        return this.authService.signUp(dto);
+    }
+
+    @Post('login')
+    login(@Body() dto: AuthDto){
+
+        return this.authService.login(dto);
+
+
+    }
+
+
+
+}
--- secure-connect-backend/src/auth/auth.service.ts
+++ secure-connect-backend/src/auth/auth.service.ts
@@ -1,4 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import {ForbiddenException, Injectable} from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
+import { JwtService } from '@nestjs/jwt';
+import {AuthDto} from "./AuthDTO";
+import * as argon from 'argon2';
+import {PrismaClientKnownRequestError} from "@prisma/client/runtime/client";
+import {catchError} from "rxjs";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService
+    ){}
+
+
+    async signUp(dto: AuthDto){
+
+        try {
+            const hash = await argon.hash(dto.password);
+
+            const user = await this.prisma.user.create({
+                data: {
+                    username: dto.username,
+                    password: hash
+                },
+                select: {
+                    id: true,
+                    username: true,
+                }
+            });
+
+
+
+            //return the user here.
+
+        }catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
+            }
+            throw error;
+        }
+    }
+
+
+    async login(dto: AuthDto){
+
+        try{
+            const user = await this.prisma.user.findUniqueOrThrow({
+                where: {
+                    username: dto.username
+                }
+            });
+
+            //check of the password compatibility
+
+            const {password, ...safeUser} = user;
+            // return the user
+
+
+
+
+
+        }catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2025') {
+                    throw new ForbiddenException('User not found');
+                }
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
